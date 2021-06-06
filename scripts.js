4bcb9710a7c1465f9059acb6447b8eb567b09a78
rhearamaiya <ramaiyarhea@gmail.com>
2021-06-05 19:20:35 -0500

    finish vehicle

--- scripts.js
+++ scripts.js
@@ -165,100 +165,38 @@ selectType.addEventListener('change', (event) => {
 				}
 				
 		}
-	
-	});
 
+		if(emissionType==="vehicle"){
+			document.querySelector('#values').innerHTML = 
+			`
+			<input type="text" id="distance_value" value="" placeholder="Enter mileage">
+			`
+			document.querySelector('#btn').onclick = ev => {
 
-//////////////////////////////////////////
-// document.querySelector('#btn').onclick = ev => {
+				var distance_value=document.getElementById("distance_value").value;
 
-// // FOR FLIGHT //
+				const vehicle_data = {
+					"type": "vehicle",
+					"distance_unit": "mi",
+					"distance_value": distance_value,
+					"vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"
+					}
+					fetch(`https://www.carboninterface.com/api/v1/estimates`,{
+						method: 'POST',
+						headers: {
+							'Authorization':'Bearer pt7oRhb0oW4CSc7IHKIDg',
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify(vehicle_data)
+					})
+						.then(response => response.json())
+						.then(result => {
+							//console.log('data:', result.data.attributes.carbon_g);
+							//type of car is in response too
+							document.querySelector('#all_results').innerHTML=clear
+							document.querySelector('#vehicle_estimate').innerHTML = `Carbon released from ${distance_value}mi by 1993 Toyota Corolla: ${result.data.attributes.carbon_g} grams`
+						});
 
-// if(document.querySelector('#choose-type').value === "flight")
-// {
-// var val=document.getElementById("value").value
-// const flight_data = {
-// 	"type": "flight",
-// 	"passengers": val,
-// 	"legs": [
-// 		{"departure_airport": "sfo", "destination_airport": "yyz"},
-// 		{"departure_airport": "yyz", "destination_airport": "sfo"}
-// 	]
-// }
-// fetch(`https://www.carboninterface.com/api/v1/estimates`,{
-// 	method: 'POST',
-// 	headers: {
-// 		'Authorization':'Bearer pt7oRhb0oW4CSc7IHKIDg',
-// 		'Content-Type': 'application/json'
-// 	},
-// 	body: JSON.stringify(flight_data)
-// })
-// 	.then(response => response.json())
-// 	.then(result => {
-// 		//console.log('data:', result.data.attributes.carbon_g);
-// 		document.querySelector('#all_results').innerHTML=clear
-// 		document.querySelector('#flight_estimate').innerHTML = `Carbon released from round-trip, ${val}-passenger flight San Francisco International Airport
-// 		to Toronto Pearson International Airport: ${result.data.attributes.carbon_g} grams`
-// 	});
-// }
-
-// // FOR SHIPPING //
-
-// if(document.querySelector('#choose-type').value === "shipping")
-// {
-// var val=document.getElementById("value").value
-
-// const shipping_data = {
-// 	"type": "shipping",
-// 	"weight_value": val,
-// 	"weight_unit": "g",
-// 	"distance_value": 2000,
-// 	"distance_unit": "km",
-// 	"transport_method": "truck"
-// }
-// fetch(`https://www.carboninterface.com/api/v1/estimates`,{
-// 	method: 'POST',
-// 	headers: {
-// 		'Authorization':'Bearer pt7oRhb0oW4CSc7IHKIDg',
-// 		'Content-Type': 'application/json'
-// 	},
-// 	body: JSON.stringify(shipping_data)
-// })
-// 	.then(response => response.json())
-// 	.then(result => {
-// 		//console.log('data:', result.data.attributes.carbon_g);
-// 		document.querySelector('#all_results').innerHTML=clear
-// 		document.querySelector('#shipping_estimate').innerHTML = `Carbon released from ${val}g shipment for 2000km by truck: ${result.data.attributes.carbon_g} grams`
-// 	});
-// }
-
-// // FOR VEHICLE //
-
-// if(document.querySelector('#choose-type').value === "vehicle")
-// {
-// var val=document.getElementById("value").value
-// const vehicle_data = {
-// "type": "vehicle",
-// "distance_unit": "mi",
-// "distance_value": val,
-// "vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"
-// }
-// fetch(`https://www.carboninterface.com/api/v1/estimates`,{
-// 	method: 'POST',
-// 	headers: {
-// 		'Authorization':'Bearer pt7oRhb0oW4CSc7IHKIDg',
-// 		'Content-Type': 'application/json'
-// 	},
-// 	body: JSON.stringify(vehicle_data)
-// })
-// 	.then(response => response.json())
-// 	.then(result => {
-// 		//console.log('data:', result.data.attributes.carbon_g);
-// 		//type of car is in response too
-// 		document.querySelector('#all_results').innerHTML=clear
-// 		document.querySelector('#vehicle_estimate').innerHTML = `Carbon released from ${val}mi by 1993 Toyota Corolla: ${result.data.attributes.carbon_g} grams`
-// 	});
-// }
-// };
-
-// });
+					}
+				}
+});
