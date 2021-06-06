2bc444a291382b593419d3987d1b5915547edd58
rhearamaiya <ramaiyarhea@gmail.com>
2021-06-05 19:32:01 -0500

    added all states to electricity

--- scripts.js
+++ scripts.js
@@ -47,13 +47,65 @@ selectType.addEventListener('change', (event) => {
 		`
 		<input type="text" id="value" value="" placeholder="Enter megawatt hours">
 		<br>
-		<input type="text" id="state" value="" placeholder="Enter state">
+		<select id = "state">
+	<option value="AL">Alabama</option>
+	<option value="AK">Alaska</option>
+	<option value="AZ">Arizona</option>
+	<option value="AR">Arkansas</option>
+	<option value="CA">California</option>
+	<option value="CO">Colorado</option>
+	<option value="CT">Connecticut</option>
+	<option value="DE">Delaware</option>
+	<option value="DC">District Of Columbia</option>
+	<option value="FL">Florida</option>
+	<option value="GA">Georgia</option>
+	<option value="HI">Hawaii</option>
+	<option value="ID">Idaho</option>
+	<option value="IL" selected>Illinois</option>
+	<option value="IN">Indiana</option>
+	<option value="IA">Iowa</option>
+	<option value="KS">Kansas</option>
+	<option value="KY">Kentucky</option>
+	<option value="LA">Louisiana</option>
+	<option value="ME">Maine</option>
+	<option value="MD">Maryland</option>
+	<option value="MA">Massachusetts</option>
+	<option value="MI">Michigan</option>
+	<option value="MN">Minnesota</option>
+	<option value="MS">Mississippi</option>
+	<option value="MO">Missouri</option>
+	<option value="MT">Montana</option>
+	<option value="NE">Nebraska</option>
+	<option value="NV">Nevada</option>
+	<option value="NH">New Hampshire</option>
+	<option value="NJ">New Jersey</option>
+	<option value="NM">New Mexico</option>
+	<option value="NY">New York</option>
+	<option value="NC">North Carolina</option>
+	<option value="ND">North Dakota</option>
+	<option value="OH">Ohio</option>
+	<option value="OK">Oklahoma</option>
+	<option value="OR">Oregon</option>
+	<option value="PA">Pennsylvania</option>
+	<option value="RI">Rhode Island</option>
+	<option value="SC">South Carolina</option>
+	<option value="SD">South Dakota</option>
+	<option value="TN">Tennessee</option>
+	<option value="TX">Texas</option>
+	<option value="UT">Utah</option>
+	<option value="VT">Vermont</option>
+	<option value="VA">Virginia</option>
+	<option value="WA">Washington</option>
+	<option value="WV">West Virginia</option>
+	<option value="WI">Wisconsin</option>
+	<option value="WY">Wyoming</option>
+</select>				
 		`
 
 		document.querySelector('#btn').onclick = ev => {
 
 			var val=document.getElementById("value").value;
-			var state=document.getElementById("state").value;
+			var state=document.getElementById("state").value.toLowerCase();
 
 			var electricity_data = {
 			"type": "electricity",
@@ -77,7 +129,7 @@ selectType.addEventListener('change', (event) => {
 				// console.log("result:", electricity_data)
 				// console.log(typeof val)
 				document.querySelector('#all_results').innerHTML=clear
-				document.querySelector('#electricity_estimate').innerHTML = `Carbon released from ${val} megawatt hours of electricity in Florida: ${result.data.attributes.carbon_g} grams`
+				document.querySelector('#electricity_estimate').innerHTML = `Carbon released from ${val} megawatt hours of electricity in ${document.getElementById("state").value}: ${result.data.attributes.carbon_g} grams`
 			
 				})
 
